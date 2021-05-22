8973d9a9971fd0859ecfd59d7a7122f5d9ae5299
thedejaware <mehmet.akin84@gmail.com>
2021-05-22 22:18:56 +0300

    - Project with Unit Test Template has been created

---
+++ src/Tests/CER.Tests/CuriousRoverTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CER.Tests
+{
+    [TestClass]
+    public class CuriousRoverTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
